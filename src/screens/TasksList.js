8cafb1a98b1172189531ca843aefb9dac28e83c2
Henrique <henrique.thsantos.ths@gmail.com>
2021-04-29 10:14:04 -0300

    Componente Lista de Tarefas #2

--- src/screens/TasksList.js
+++ src/screens/TasksList.js
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
-import { View, Text, StatusBar } from "react-native";
+import {
+    SafeAreaView,
+    View,
+    Text,
+    StatusBar,
+    ImageBackground,
+    StyleSheet,
+} from "react-native";
 
+import todayImage from "../../assets/imgs/today.jpg";
 export default class TaskList extends Component {
     render() {
         return (
-            <View>
+            <SafeAreaView style={styles.container}>
                 <StatusBar />
-                <Text>TaskList</Text>
-            </View>
+                <ImageBackground
+                    source={todayImage}
+                    style={styles.background}
+                ></ImageBackground>
+                <View style={styles.taskList}>
+                    <Text>TaskList</Text>
+                </View>
+            </SafeAreaView>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    background: {
+        flex: 3,
+    },
+    taskList: {
+        flex: 7,
+    },
+});
