75b164638b803ab4c42685114fbd32dbc025caa1
Henrique <henrique.thsantos.ths@gmail.com>
2021-04-29 17:00:17 -0300

    Componente Lista de tarefas #4

--- src/screens/TasksList.js
+++ src/screens/TasksList.js
@@ -9,27 +9,71 @@ import {
 } from "react-native";
 
 import todayImage from "../../assets/imgs/today.jpg";
+import commonStyles from "../commonStyles";
 
 import moment from "moment";
 import "moment/locale/pt-br";
 
+import * as Font from "expo-font";
+import {
+    Lato_100Thin,
+    Lato_300Light,
+    Lato_400Regular,
+    Lato_900Black_Italic,
+} from "@expo-google-fonts/lato";
+
+let customFonts = {
+    Lato_100Thin,
+    Lato_300Light,
+    Lato_400Regular,
+    Lato_900Black_Italic,
+};
+
 export default class TaskList extends Component {
+    state = {
+        fontsLoaded: false,
+    };
+
+    async _loadFontsAsync() {
+        await Font.loadAsync(customFonts);
+        this.setState({ fontsLoaded: true });
+    }
+
+    componentDidMount() {
+        this._loadFontsAsync();
+    }
+
     render() {
         const today = moment().locale("pt-br").format("ddd, D [de] MMMM YYYY");
-        return (
-            <SafeAreaView style={styles.container}>
-                <StatusBar />
-                <ImageBackground source={todayImage} style={styles.background}>
-                    <View style={styles.titleBar}>
-                        <Text>Hoje</Text>
-                        <Text>{today}</Text>
+
+        if (this.state.fontsLoaded) {
+            return (
+                <SafeAreaView style={styles.container}>
+                    <StatusBar />
+                    <ImageBackground
+                        source={todayImage}
+                        style={styles.background}
+                    >
+                        <View style={styles.titleBar}>
+                            <Text style={[styles.title]}>Hoje</Text>
+                            <Text style={[styles.subtitle]}>{today}</Text>
+                        </View>
+                    </ImageBackground>
+                    <View style={styles.taskList}>
+                        <Text>Tarefa #1</Text>
+                        <Text>Tarefa #2</Text>
+                        <Text>Tarefa #3</Text>
+                        <Text>Tarefa #4</Text>
                     </View>
-                </ImageBackground>
-                <View style={styles.taskList}>
-                    <Text>TaskList</Text>
+                </SafeAreaView>
+            );
+        } else {
+            return (
+                <View>
+                    <Text>Fontes não carregaram</Text>
                 </View>
-            </SafeAreaView>
-        );
+            );
+        }
     }
 }
 
@@ -46,5 +90,19 @@ const styles = StyleSheet.create({
     titleBar: {
         flex: 1,
         justifyContent: "flex-end",
+    },
+    title: {
+        fontFamily: "Lato_400Regular",
+        color: commonStyles.colors.secondary,
+        fontSize: 50,
+        marginLeft: 20,
+        marginBottom: 20,
+    },
+    subtitle: {
+        fontFamily: "Lato_300Light",
+        color: commonStyles.colors.secondary,
+        fontSize: 30,
+        marginLeft: 20,
+        marginBottom: 30,
     },
 });
