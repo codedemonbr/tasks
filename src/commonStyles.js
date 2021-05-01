4a736a360aecceb60f6069d4099a211d02dbbab5
Henrique <henrique.thsantos.ths@gmail.com>
2021-05-01 09:46:58 -0300

    Modal adicionar Tarefa #2

--- src/commonStyles.js
+++ src/commonStyles.js
@@ -1,5 +1,6 @@
 export default {
     colors: {
+        today: '#b13b44',
         secondary: "#FFF",
         mainText: "#222",
         subText: "#777",
--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -4,7 +4,10 @@ import {
     View,
     StyleSheet,
     TouchableWithoutFeedback,
+    Text,
 } from "react-native";
+
+import commonStyles from "../commonStyles";
 
 export default class AddTask extends Component {
     render() {
@@ -18,6 +21,12 @@ export default class AddTask extends Component {
                 <TouchableWithoutFeedback onPress={this.props.onCancel}>
                     <View style={styles.background}></View>
                 </TouchableWithoutFeedback>
+                <View style={styles.container}>
+                    <Text style={styles.header}>Nova Tarefa</Text>
+                </View>
+                <TouchableWithoutFeedback onPress={this.props.onCancel}>
+                    <View style={styles.background}></View>
+                </TouchableWithoutFeedback>
             </Modal>
         );
     }
@@ -27,5 +36,16 @@ const styles = StyleSheet.create({
     background: {
         flex: 1,
         backgroundColor: "rgba(0, 0, 0, 0.7)",
+    },
+    container: {
+        flex: 2,
+        backgroundColor: "#fff",
+    },
+    header: {
+        backgroundColor: commonStyles.colors.today,
+        color: commonStyles.colors.secondary,
+        textAlign: "center",
+        padding: 15,
+        fontSize: 18,
     },
 });
