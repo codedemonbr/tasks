c7e57b6bd284c1dde1430e5ffeffae78c77a8386
Henrique <henrique.thsantos.ths@gmail.com>
2021-05-01 17:30:19 -0300

    Botão adicionar tarefa

--- src/screens/TasksList.js
+++ src/screens/TasksList.js
@@ -141,6 +141,17 @@ export default class TaskList extends Component {
                             )}
                         />
                     </View>
+                    <TouchableOpacity
+                        style={styles.addButton}
+                        activeOpacity={0.7}
+                        onPress={() => this.setState({ showAddTask: true })}
+                    >
+                        <FontAwesome
+                            name="plus"
+                            size={20}
+                            color={commonStyles.colors.secondary}
+                        />
+                    </TouchableOpacity>
                 </SafeAreaView>
             );
         } else {
@@ -186,5 +197,16 @@ const styles = StyleSheet.create({
         marginHorizontal: 20,
         justifyContent: "flex-end",
         marginTop: Platform.OS === "ios" ? 40 : 20,
+    },
+    addButton: {
+        position: "absolute",
+        right: 30,
+        bottom: 30,
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        backgroundColor: commonStyles.colors.today,
+        justifyContent: "center",
+        alignItems: "center",
     },
 });
