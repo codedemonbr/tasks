d47d27680ce313787c7d7d768abca2719de8dfb5
Henrique <henrique.thsantos.ths@gmail.com>
2021-05-06 15:47:29 -0300

    Tela de autenticação #2

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -6,16 +6,41 @@ import {
     View,
     TextInput,
     TouchableOpacity,
+    Platform,
 } from "react-native";
 
 import backgroundImage from "../../assets/imgs/login.jpg";
 import commonStyles from "../commonStyles";
 
 export default class Auth extends Component {
+    state = {
+        email: "",
+        password: "",
+    };
+
     render() {
         return (
             <ImageBackground source={backgroundImage} style={styles.background}>
                 <Text style={styles.title}>Tasks</Text>
+                <View style={styles.formContainer}>
+                    <TextInput
+                        placeholder="E-mail"
+                        value={this.state.email}
+                        style={styles.input}
+                        onChangeText={(email) => this.setState({ email })}
+                    />
+                    <TextInput
+                        placeholder="Password"
+                        value={this.state.password}
+                        style={styles.input}
+                        onChangeText={(password) => this.setState({ password })}
+                    />
+                    <TouchableOpacity>
+                        <View style={styles.button}>
+                            <Text style={styles.buttonText}>Enter</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </ImageBackground>
         );
     }
@@ -33,4 +58,21 @@ const styles = StyleSheet.create({
         fontSize: 70,
         marginBottom: 10,
     },
+    input: {
+        backgroundColor: "#FFF",
+        marginTop: 10,
+        padding: Platform.OS == "ios" ? 15 : 10,
+    },
+    formContainer: {
+        backgroundColor: "rgba(0,0,0,0.8)",
+        padding: 20,
+        width: "90%",
+    },
+    button: {
+        backgroundColor: "#080",
+        marginTop: 10,
+        padding: 10,
+        alignItems: "center",
+    },
+    buttonText: { color: "#FFF", fontSize: 20 },
 });
