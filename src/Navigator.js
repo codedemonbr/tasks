ee83da3151d88a41892cad2fdaa7696c0574fc4e
Henrique <henrique.thsantos.ths@gmail.com>
2021-05-17 14:20:33 -0300

    Drawer Navigator #1

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,7 +1,53 @@
+import React from "react";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createDrawerNavigator } from "react-navigation-drawer";
 
 import Auth from "./screens/Auth";
 import TaskList from "./screens/TasksList";
+
+const menuRoutes = {
+    Today: {
+        name: "Today",
+        screen: (props) => <TaskList title="Today" daysAhead={0} {...props} />,
+        navigationOptions: {
+            title: "Today",
+        },
+    },
+    Tomorrow: {
+        name: "Tomorrow",
+        screen: (props) => (
+            <TaskList title="Tomorrow" daysAhead={1} {...props} />
+        ),
+        navigationOptions: {
+            title: "Tomorrow",
+        },
+    },
+    Week: {
+        name: "Week",
+        screen: (props) => <TaskList title="Week" daysAhead={7} {...props} />,
+        navigationOptions: {
+            title: "Week",
+        },
+    },
+    FortNight: {
+        name: "FortNight",
+        screen: (props) => (
+            <TaskList title="FortNight" daysAhead={15} {...props} />
+        ),
+        navigationOptions: {
+            title: "FortNight",
+        },
+    },
+    Month: {
+        name: "Month",
+        screen: (props) => <TaskList title="Month" daysAhead={30} {...props} />,
+        navigationOptions: {
+            title: "Month",
+        },
+    },
+};
+
+const menuNavigator = createDrawerNavigator(menuRoutes);
 
 const mainRoutes = {
     Auth: {
@@ -10,12 +56,11 @@ const mainRoutes = {
     },
     Home: {
         name: "Home",
-        screen: TaskList,
+        screen: menuNavigator,
     },
 };
 
 const mainNavigator = createSwitchNavigator(mainRoutes, {
     initialRouteName: "Auth",
 });
-
 export default createAppContainer(mainNavigator);
--- src/screens/TasksList.js
+++ src/screens/TasksList.js
@@ -53,7 +53,9 @@ export default class TaskList extends Component {
 
     loadTasks = async () => {
         try {
-            const maxDate = moment().format("YYYY-MM-DD 23:59:59");
+            const maxDate = moment()
+                .add({ days: this.props.daysAhead })
+                .format("YYYY-MM-DD 23:59:59");
             const res = await axios.get(`${server}/tasks?date=${maxDate}`);
             this.setState({ tasks: res.data }, this.filterTasks);
         } catch (e) {
@@ -107,7 +109,8 @@ export default class TaskList extends Component {
                 desc: newTask.desc,
                 estimateAt: newTask.date,
             });
-            this.setState({ showDoneTasks: false }, this.loadTasks);
+
+            this.setState({ showAddTask: false }, this.loadTasks);
         } catch (e) {
             showError(e);
         }
@@ -135,6 +138,15 @@ export default class TaskList extends Component {
                 />
                 <ImageBackground source={todayImage} style={styles.background}>
                     <View style={styles.iconBar}>
+                        <TouchableOpacity
+                            onPress={() => this.props.navigation.openDrawer()}
+                        >
+                            <Icon
+                                name="bars"
+                                size={20}
+                                color={commonStyles.colors.secondary}
+                            />
+                        </TouchableOpacity>
                         <TouchableOpacity onPress={this.toggleFilter}>
                             <Icon
                                 name={
@@ -148,7 +160,7 @@ export default class TaskList extends Component {
                         </TouchableOpacity>
                     </View>
                     <View style={styles.titleBar}>
-                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.title}>{this.props.title}</Text>
                         <Text style={styles.subtitle}>{today}</Text>
                     </View>
                 </ImageBackground>
@@ -210,7 +222,7 @@ const styles = StyleSheet.create({
     iconBar: {
         flexDirection: "row",
         marginHorizontal: 20,
-        justifyContent: "flex-end",
+        justifyContent: "space-between",
         marginTop: Platform.OS === "ios" ? 40 : 20,
     },
     addButton: {
