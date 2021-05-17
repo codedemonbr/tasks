e57024f88a04fcc5c6d3c1953ca23e736ec8a43f
Henrique <henrique.thsantos.ths@gmail.com>
2021-05-17 17:57:20 -0300

    Drawer Navigator #4

--- src/Navigator.js
+++ src/Navigator.js
@@ -5,6 +5,25 @@ import { createDrawerNavigator } from "react-navigation-drawer";
 import Auth from "./screens/Auth";
 import TaskList from "./screens/TasksList";
 
+import Menu from "./screens/Menu";
+import commonStyles from "./commonStyles";
+
+const menuConfig = {
+    initialRouteName: "Today",
+    contentComponent: Menu,
+    contentOptions: {
+        labelStyle: {
+            // fontFamily: commonStyles.fontFamily,
+            fontWeight: "normal",
+            fontSize: 20,
+        },
+        activeLabelStyle: {
+            color: "#080",
+            fontWeight: "bold",
+        },
+    },
+};
+
 const menuRoutes = {
     Today: {
         name: "Today",
@@ -47,7 +66,7 @@ const menuRoutes = {
     },
 };
 
-const menuNavigator = createDrawerNavigator(menuRoutes);
+const menuNavigator = createDrawerNavigator(menuRoutes, menuConfig);
 
 const mainRoutes = {
     Auth: {
